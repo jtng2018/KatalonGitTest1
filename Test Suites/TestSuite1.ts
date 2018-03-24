3aee3ea0e88cb5e1acdff0651c638dd314c87aed
jtng2018 <jtng2018@gmail.com>
2018-03-24 16:43:59 +0800

    Added changes

--- Test Suites/TestSuite1.ts
+++ Test Suites/TestSuite1.ts
@@ -4,11 +4,23 @@
    <name>TestSuite1</name>
    <tag></tag>
    <isRerun>false</isRerun>
-   <lastRun>2018-03-24T15:32:44</lastRun>
+   <lastRun>2018-03-24T16:42:57</lastRun>
    <mailRecipient></mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <testSuiteGuid>bf4b1faf-4069-4dab-9297-5029c7a6dde0</testSuiteGuid>
+   <testCaseLink>
+      <guid>513d83b0-ede2-41c0-93b1-22e2acae90ad</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test1</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e0de3b7d-8693-4376-98e4-c9d57bfc5de8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test2</testCaseId>
+   </testCaseLink>
 </TestSuiteEntity>
