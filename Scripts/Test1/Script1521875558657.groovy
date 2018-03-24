b71f0d932eee759c84332072006fc4b8b363ba33
jtng2018 <jtng2018@gmail.com>
2018-03-24 15:54:04 +0800

    Added changes

--- Scripts/Test1/Script1521875558657.groovy
+++ Scripts/Test1/Script1521875558657.groovy
@@ -19,3 +19,7 @@ import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUiBuiltInKe
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
 
+WebUI.openBrowser('http://opensource.demo.orangehrmlive.com/index.php/auth/login')
+
+WebUI.closeBrowser()
+
---
+++ Test Suites/TestSuite1.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-03-24T15:32:44</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>bf4b1faf-4069-4dab-9297-5029c7a6dde0</testSuiteGuid>
+</TestSuiteEntity>
